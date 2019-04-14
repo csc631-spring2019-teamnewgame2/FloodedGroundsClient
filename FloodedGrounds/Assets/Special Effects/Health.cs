10d196045636fb6109249c92192b0e79cf7af9c7
asarmiento1 <andrewsarmiento12@gmail.com>
2019-04-14 13:50:38 -0700

    Game Logic Part 1
    Added health reaches zero, trigger death animation. And respawn player object.

---
+++ FloodedGrounds/Assets/Special Effects/Health.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthbar;
+    Animator anim;
+
+    public SkinnedMeshRenderer rend;
+    public Canvas canvas;
+
+    public void takeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            anim.SetBool("isDead", true);
+            Debug.Log("Dead");
+            StartCoroutine(Respawn());
+        }
+
+        healthbar.sizeDelta = new Vector2(currentHealth, healthbar.sizeDelta.y);
+    }
+    
+    IEnumerator Respawn()
+    {
+        canvas.enabled = false;
+        yield return new WaitForSeconds(2);
+        rend.enabled = false;
+        yield return new WaitForSeconds(3);
+        this.transform.position = new Vector3(8, 0, 8);
+        anim.SetBool("isDead", false);
+        anim.Play("idle");
+        currentHealth = 100;
+        healthbar.sizeDelta = new Vector2(currentHealth, healthbar.sizeDelta.y);
+        canvas.enabled = true;
+        rend.enabled = true;
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rend = GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+}
