1f451e1f6c382f5a7f7d7c7c015fe5efad298e08
asarmiento1 <andrewsarmiento12@gmail.com>
2019-04-30 03:24:27 -0700

    Integrated interactable to this branch scene A

---
+++ FloodedGrounds/Assets/HUD/Scripts/ManagementHUD.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagementHUD : MonoBehaviour
+{
+    public int ammoIn = 30;
+    public int ammoOut = 120;
+
+    public int _medPack = 1;
+
+    public Text ammoDisplay;
+    public Text medpack;
+
+    public void AmmoCounter(int amount)
+    {
+        ammoIn -= amount;
+
+        if(ammoIn <= 0 && ammoOut <= 0)
+        {
+            ammoIn = 0;
+            ammoOut = 0;
+            Debug.Log("Out of ammo! Find more ammo!");
+        }
+        else if(ammoIn <= 0 && ammoOut > 0)
+        {
+            ammoIn = 0;
+            Debug.Log("Reload!");
+        }
+
+        ammoDisplay.text = ammoIn.ToString() + " <size=8>/ " + ammoOut.ToString() + "</size>";
+    }
+
+    public void AmmoReload()
+    {
+        if(ammoIn > 0 && ammoOut >= 30)
+        {
+            ammoOut = ammoOut - (30 - ammoIn);
+            ammoIn = 30;
+        }
+        if(ammoIn == 0 && ammoOut >= 30)
+        {
+            ammoIn = 30;
+            ammoOut -= 30;
+        }
+        if(ammoIn > 0 && ammoOut < 30 && ammoIn + ammoOut <= 30)
+        {
+            if(ammoIn + ammoOut == 30)
+            {
+                ammoIn = 30;
+                ammoOut = 0;
+            }
+            else
+            {
+                ammoIn = ammoIn + ammoOut;
+                ammoOut = 0;
+            }
+        }
+        if(ammoIn == 0 && ammoOut < 30)
+        {
+            ammoIn = ammoOut;
+            ammoOut = 0;
+        }
+
+        ammoDisplay.text = ammoIn.ToString() + " <size=8>/ " + ammoOut.ToString() + "</size>";
+    }
+
+    public void MedCounter(int add)
+    {
+        _medPack += add;
+
+        medpack.text = _medPack.ToString();
+    }
+
+    public void AmmoPickup(int add)
+    {
+        ammoOut += add;
+
+        ammoDisplay.text = ammoIn.ToString() + " <size=8>/ " + ammoOut.ToString() + "</size>";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ FloodedGrounds/Assets/HUD/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour
+{
+    //public GameObject _medpack, _smokeGrenade, grenade;
+    public RaycastHit hit;
+    public Ray ray;
+    public ManagementHUD counter;
+
+    public bool medPack;
+    public float maxDistance = 5;
+
+    //private Vector3 raycastPos;
+    
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        medPack = false;
+
+        //foreach (Transform t in transform.GetComponentInChildren<Transform>(true))
+        //    if (t.gameObject.name == "CC_Base_Eye")
+        //        raycastPos = t.gameObject.transform.position;
+
+        //counter = GetComponentInParent<ManagementHUD>();
+        //smokeGrenade = false;
+        //grenade = false;
+        //var screen
+        //ray = cam.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * maxDistance, Color.magenta);
+
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, maxDistance) && hit.collider.gameObject.tag == "Medpack")
+        {
+            //isInteractable = true;
+            //medPack = true;
+            //Debug.Log("Medpack is interactable");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(hit.collider.gameObject);
+                //medPack = false;
+                counter.MedCounter(1);
+            }
+        }
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, maxDistance) && hit.collider.gameObject.tag == "Ammo")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(hit.collider.gameObject);
+                counter.AmmoPickup(120);
+            }
+        }
+        /*if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, maxDistance) && hit.collider.gameObject.tag == "SmokeGrenade")
+        {
+            //isInteractable = true;
+            smokeGrenade = true;
+            //Debug.Log("Smoke Grenade is interactable");
+        }
+        else if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, maxDistance) && hit.collider.gameObject.tag == "Grenade")
+        {
+            //isInteractable = true;
+            grenade = true;
+            //Debug.Log("Grenade is interactable");
+        }*/
+    }
+}
