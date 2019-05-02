e73fd737dc55de8d0eb1519dba719c348fe5e49f
Tina <TinaNguyen290@yahoo.com>
2019-05-02 01:33:34 -0700

    Finished coding the feet movements using only hdir and vdir
    Will need to add running animation

--- FloodedGrounds/Assets/All_Scripts/Charactermovement.cs
+++ FloodedGrounds/Assets/All_Scripts/Charactermovement.cs
@@ -19,13 +19,16 @@ public class Charactermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Inputs();
         Movement();
         
     }
 
     void Inputs()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            anim.SetTrigger("isRunning");
+        }
     }
 
     void Movement()
@@ -33,26 +36,31 @@ public class Charactermovement : MonoBehaviour
         //Left/Right/Up/Down Input
         float hdir = Input.GetAxis("Horizontal");
         float vdir = Input.GetAxis("Vertical");
-        if (Input.GetKeyUp(KeyCode.W) && Input.GetKeyUp(KeyCode.A) && Input.GetKeyUp(KeyCode.S) && Input.GetKeyUp(KeyCode.D))
-        {
-            hdir = 0f;
-            vdir = 0f;
-        }
-
-        Debug.Log("hdir: " + hdir + "\n" + "vdir: " + vdir);
-
-        MoveAnimations(hdir,vdir);
 
         //Moving the character
         Vector3 direction = new Vector3(hdir, 0, vdir);
         Vector3 velocity = direction * moveSpeed * Time.deltaTime;
         cc.Move(velocity);
+
+        if (!Input.GetKeyUp(KeyCode.W) && Input.GetKeyUp(KeyCode.A) && Input.GetKeyUp(KeyCode.S) && Input.GetKeyUp(KeyCode.D))
+        {
+            hdir = 0.0f;
+            vdir = 0.0f;
+        }
+
+
+        Debug.Log("hdir: " + hdir + "\n" + "vdir: " + vdir);
+
+        MoveAnimations(hdir,vdir);
+
+
+        //hdir = 0.0f;
+        //vdir = 0.0f;
     }
 
     void MoveAnimations(float hdir, float vdir)
     {
-        //hdir = Mathf.Round(hdir);
-        //vdir = Mathf.Round(hdir);
+
         anim.SetFloat("horizontal", hdir);
         anim.SetFloat("vertical", vdir);
 
