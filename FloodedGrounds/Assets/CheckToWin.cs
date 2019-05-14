2501d032716106c519ae04b723f21b51e4cb1246
asarmiento1 <andrewsarmiento12@gmail.com>
2019-05-14 12:36:23 -0700

    Working on playable area

---
+++ FloodedGrounds/Assets/CheckToWin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckToWin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Izzy" || other.gameObject.tag == "Winston" || other.gameObject.tag == "Max")
+        {
+
+        }
+    }
+}
